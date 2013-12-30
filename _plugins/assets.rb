e2aca78e692c0d84b673b9d240ea8c1e1f6abd35
Aaron Kalin <akalin@martinisoftware.com>
2013-12-29 18:55:07 -0600

    Include font-awesome icons

--- _plugins/assets.rb
+++ _plugins/assets.rb
@@ -1,1 +1,5 @@
 require "jekyll-assets"
+
+font_awesome_root = Gem::Specification.find_by_name("font-awesome-sass").gem_dir
+Sprockets.append_path File.join(font_awesome_root, "vendor", "assets", "stylesheets")
+Sprockets.append_path File.join(font_awesome_root, "vendor", "assets", "fonts")
