da2b7b56962b5973b40344711a40aec24e9fdad5
Aaron Kalin <akalin@martinisoftware.com>
2014-08-31 20:34:45 -0500

    Update Font-Awesome sprockets integration

--- _plugins/assets.rb
+++ _plugins/assets.rb
@@ -1,5 +1,6 @@
 require "jekyll-assets"
 
-font_awesome_root = Gem::Specification.find_by_name("font-awesome-sass").gem_dir
-Sprockets.append_path File.join(font_awesome_root, "vendor", "assets", "stylesheets")
-Sprockets.append_path File.join(font_awesome_root, "vendor", "assets", "fonts")
+fa = Gem::Specification.find_by_name("font-awesome-sass").gem_dir
+%w[fonts/font-awesome stylesheets].each do |asset|
+  Sprockets.append_path File.join(fa, "assets", asset)
+end
