e2a8aa6a8a2c9181b1c4771d2551ce7401d689ab
Aaron Kalin <akalin@martinisoftware.com>
2016-01-03 09:07:13 -0600

    Remove asset plugin hack

--- _plugins/assets.rb
+++
@@ -1,6 +0,0 @@
-require "jekyll-assets"
-
-fa = Gem::Specification.find_by_name("font-awesome-sass").gem_dir
-%w[fonts/font-awesome stylesheets].each do |asset|
-  Sprockets.append_path File.join(fa, "assets", asset)
-end
